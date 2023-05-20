8f655757c9aeb42c51b295cddb9f72300284da50
Bratet <ahmedmrabet.002@gmail.com>
2023-05-21 00:47:03 +0100

    used K fold for evaluation

--- src/models/cuckoo_search_nn.py
+++ src/models/cuckoo_search_nn.py
@@ -41,14 +41,15 @@ def fitness(solution, X, y, n_inputs, n_hidden, n_outputs):
     y_pred = np.argmax(output, axis=1)
     return -accuracy_score(y, y_pred)
 
-# Define the Cuckoo Search algorithm
 def cuckoo_search(X, y, n_pop, n_iterations, pa, n_inputs, n_hidden, n_outputs, solution_size):
-    # Initialize population
+    # Initialize population and fitness
     population = np.random.uniform(-1, 1, (n_pop, solution_size))
-
-    # Perform the Cuckoo Search algorithm
-    best_solution = population[np.argmin([fitness(sol, X, y, n_inputs, n_hidden, n_outputs) for sol in population])]
-    best_fitness = fitness(best_solution, X, y, n_inputs, n_hidden, n_outputs)
+    fitnesses = np.array([fitness(sol, X, y, n_inputs, n_hidden, n_outputs) for sol in population])
+    
+    # Find the best solution and its fitness
+    min_idx = np.argmin(fitnesses)
+    best_solution = population[min_idx]
+    best_fitness = fitnesses[min_idx]
 
     for _ in range(n_iterations):
         # Generate a cuckoo solution
@@ -61,21 +62,22 @@ def cuckoo_search(X, y, n_pop, n_iterations, pa, n_inputs, n_hidden, n_outputs, 
 
         # Select a random solution from the population
         random_idx = np.random.randint(n_pop)
-        random_solution = population[random_idx]
-        random_fitness = fitness(random_solution, X, y, n_inputs, n_hidden, n_outputs)
 
         # Replace the random solution with the cuckoo solution if it has better fitness
-        if cuckoo_fitness < random_fitness:
+        if cuckoo_fitness < fitnesses[random_idx]:
             population[random_idx] = cuckoo
+            fitnesses[random_idx] = cuckoo_fitness
 
         # Replace a fraction pa of the worst solutions with new randomly generated solutions
         n_replace = int(n_pop * pa)
-        worst_indices = np.argsort([fitness(sol, X, y, n_inputs, n_hidden, n_outputs) for sol in population])[-n_replace:]
+        worst_indices = np.argsort(fitnesses)[-n_replace:]
         population[worst_indices] = np.random.uniform(-1, 1, (n_replace, solution_size))
+        fitnesses[worst_indices] = np.array([fitness(sol, X, y, n_inputs, n_hidden, n_outputs) for sol in population[worst_indices]])
 
         # Update the best solution found so far
-        current_best_solution = population[np.argmin([fitness(sol, X, y, n_inputs, n_hidden, n_outputs) for sol in population])]
-        current_best_fitness = fitness(current_best_solution, X, y, n_inputs, n_hidden, n_outputs)
+        min_idx = np.argmin(fitnesses)
+        current_best_solution = population[min_idx]
+        current_best_fitness = fitnesses[min_idx]
 
         if current_best_fitness < best_fitness:
             best_solution = current_best_solution
--- src/utils/methods_utils.py
+++ src/utils/methods_utils.py
@@ -15,11 +15,3 @@ def one_hot_encode(y, num_classes):
 # Define the accuracy score function
 def accuracy_score(y_true, y_pred):
     return np.sum(y_true == y_pred) / len(y_true)
-
-# define train_test_split function to split data into train and test sets
-def train_test_split(X, y, test_size=0.2):
-    indices = np.random.permutation(len(X))
-    test_size = int(len(X) * test_size)
-    test_indices = indices[:test_size]
-    train_indices = indices[test_size:]
-    return X[train_indices], X[test_indices], y[train_indices], y[test_indices]
